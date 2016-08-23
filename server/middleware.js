ce6379beb00fbd13b8f93710ef49f795c127a2a1
natoen <hgmatsuda@gmail.com>
2016-08-24 04:07:12 +0900

    (feat) Add server middlewares for parsing

---
+++ server/middleware.js
@@ -0,0 +1,10 @@
+import bodyParser from 'body-parser';
+import logger from 'morgan';
+
+module.exports = (app, express) => {
+  app.use(logger('dev'));
+  app.use(bodyParser.json());
+  app.use(bodyParser.urlencoded({ extended: true }));
+
+  app.use(express.static(`${__dirname}/../`));
+};
