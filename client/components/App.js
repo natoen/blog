3dac1a16c02bd168711fb4ee92216ddb44b19f70
natoen <hgmatsuda@gmail.com>
2016-08-27 07:24:32 +0900

    (feat) Add styles in parent component and rename files to .jsx

--- client/components/App.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export default function App(props) {
-  return <div>{props.children}</div>;
-}
-
-App.propTypes = {
-  children: React.PropTypes.element,
-};
---
+++ client/components/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+export default function App(props) {
+  return (
+    <div
+      className="container m-x-auto"
+      style={{ minWidth: 320, maxWidth: 600 }}
+    >
+      {props.children}
+    </div>
+  );
+}
+
+App.propTypes = {
+  children: React.PropTypes.element,
+};
